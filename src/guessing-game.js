e88f99b55d24baed330f981b9041b9ea0fefda01
Kotec <vivalastelmah@gmail.com>
2023-02-28 00:09:58 +0300

    feat: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.guessResult = 0;
     }
 
     guess() {
-
+        this.guessResult = Math.round((this.min + this.max) / 2);
+        return this.guessResult
     }
 
     lower() {
-
+        this.max = this.guess()
     }
 
     greater() {
-
+        this.min = this.guess()
     }
 }
 
